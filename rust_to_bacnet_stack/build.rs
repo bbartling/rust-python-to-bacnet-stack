969ea1b833dfc122323138357b6471ed1310b1be
Ben Bartling <ben.bartling@gmail.com>
2024-01-26 15:04:10 -0600

    refactoring

--- rust_to_bacnet_stack/build.rs
+++ rust_to_bacnet_stack/build.rs
@@ -1,4 +1,6 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-search=native=../"); // Link to the parent directory
-    println!("cargo:rustc-link-lib=bacrp");        // Link to 'libbacrp.so'
+    println!("cargo:rustc-link-search=native={}", env::current_dir().unwrap().join("lib").display());
+    println!("cargo:rustc-link-lib=bacrp"); // Link to 'libbacrp.so'
 }
